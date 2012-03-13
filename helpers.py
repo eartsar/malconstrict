beff729fd706a8e6d7c2566aa482386a3657fbf8
Eitan Romanoff <ear7631@gmail.com>
2012-03-12 21:33:59 -0700

    Wraps all functions, still much to do to make things nicer

--- helpers.py
+++ helpers.py
@@ -1,39 +1,71 @@
 import json
-import inspect
 import malapi as mal
 import models
+
 
 def json_to_anime(data):
     anime = models.Anime()
     d = json.loads(data)
-    members = inspect.getmembers(anime)
     for key in d.keys():
         setattr(anime, key, d[key])
     return anime
 
 
+def json_to_list_of_anime(data):
+    anime_list = []
+    entries = json.loads(data)
+    for entry in entries:
+        temp_anime = models.Anime()
+        for key in entry.keys():
+            setattr(temp_anime, key, entry[key])
+        anime_list.append(temp_anime)
+    return anime_list
+
+
 def json_to_anime_list(data):
     anime_list = models.AnimeList()
+    anime_list.anime = []
     d = json.loads(data)
-    members = inspect.getmembers(anime_list)
-    for key in d.keys():
-        setattr(anime_list, key, d[key])
+    entries = d['anime']
+    anime_list.statistics = d['statistics']
+    
+    for entry in entries:
+        anime = models.Anime()
+        for key in entry.keys():
+            setattr(anime, key, entry[key])
+        anime_list.anime.append(anime)
     return anime_list
 
 
 def json_to_manga(data):
     manga = models.Manga()
     d = json.loads(data)
-    members = inspect.getmembers(manga)
     for key in d.keys():
         setattr(manga, key, d[key])
     return manga
 
 
+def json_to_list_of_manga(data):
+    manga_list = []
+    entries = json.loads(data)
+    for entry in entries:
+        temp_manga = models.Manga()
+        for key in entry.keys():
+            setattr(temp_manga, key, entry[key])
+        manga_list.append(temp_manga)
+    return manga_list
+
+
 def json_to_manga_list(data):
-    manga_list = models.AnimeList()
+    manga_list = models.MangaList()
+    manga_list.manga = []
     d = json.loads(data)
-    members = inspect.getmembers(manga_list)
-    for key in d.keys():
-        setattr(manga_list, key, d[key])
+    entries = d['manga']
+    manga_list.statistics = d['statistics']
+
+    for entry in entries:
+        manga = models.Manga()
+        for key in entry.keys():
+            setattr(manga, key, entry[key])
+        manga_list.manga.append(manga)
     return manga_list
--- malapi.py
+++ malapi.py
@@ -1,6 +1,8 @@
 import models
 import helpers
 
+import urllib2
+
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -24,7 +26,7 @@ def raw_get_anime_details(anime_id, mine=0, auth_token=None):
     This is a 'raw' function and will spit out JSON!
     
     Keyword arguments:
-    id -- the id of the anime whose details are requested
+    anime_id -- the id of the anime whose details are requested
     mine -- if 1, reflects user's data. Requires an auth_token. (default 0)
     auth_token -- a two value tuple that defines the (user, password)
     """
@@ -36,10 +38,10 @@ def raw_get_anime_details(anime_id, mine=0, auth_token=None):
         if len(auth_token) != 2:
             # print bad length error
             return response
-        response = requests.get(apiurl + '/anime/' + str(anime_id) + '?mine=' + str(mine), auth_token).content
+        response = requests.get(apiurl + '/anime/' + str(anime_id) + '?mine=' + str(mine), auth=auth_token)
     else:
-        response = requests.get(apiurl + '/anime/' + str(anime_id) + '?mine=' + str(mine)).content
-    return response
+        response = requests.get(apiurl + '/anime/' + str(anime_id) + '?mine=' + str(mine))
+    return response.content
 
 
 def raw_get_history(username):
@@ -60,7 +62,7 @@ def raw_search_anime(query):
     Keyword arguments:
     query -- The query to send to MAL's search
     """
-    response = requests.get(apiurl + '/anime/search?q=' + query)
+    response = requests.get(apiurl + '/anime/search?q=' + urllib2.quote(query))
     return response.content
 
 
@@ -111,12 +113,12 @@ def raw_get_manga_list(username):
     return response.content
 
 
-def raw_get_manga_details(id, mine=0, auth_token=None):
-    """Fetch a manga with the given manga id. This returns a manga.
+def raw_get_manga_details(manga_id, mine=0, auth_token=None):
+    """Fetch an manga with the given anime id. This returns a manga.
     This is a 'raw' function and will spit out JSON!
-    
+
     Keyword arguments:
-    id -- the id of the manga whose details are requested
+    manga_id -- the id of the manga whose details are requested
     mine -- if 1, reflects user's data. Requires an auth_token. (default 0)
     auth_token -- a two value tuple that defines the (user, password)
     """
@@ -128,10 +130,10 @@ def raw_get_manga_details(id, mine=0, auth_token=None):
         if len(auth_token) != 2:
             # print bad length error
             return response
-        response = requests.get(apiurl + '/manga/' + str(id) + '?mine=' + str(mine), auth_token).content
+        response = requests.get(apiurl + '/manga/' + str(manga_id) + '?mine=' + str(mine), auth=auth_token)
     else:
-        response = requests.get(apiurl + '/manga/' + str(id) + '?mine=' + str(mine)).content
-    return response
+        response = requests.get(apiurl + '/manga/' + str(manga_id) + '?mine=' + str(mine))
+    return response.content
 
 
 def raw_search_manga(query):
@@ -140,7 +142,7 @@ def raw_search_manga(query):
     Keyword arguments:
     query -- The query to send to MAL's search
     """
-    response = requests.get(apiurl + '/manga/search?q=' + query)
+    response = requests.get(apiurl + '/manga/search?q=' + urllib2.quote(query))
     return response.content
 
 
@@ -149,14 +151,14 @@ def get_anime_list(username):
     return helpers.json_to_anime_list(raw)
 
 
-def get_anime_details(id, mine=0, auth_token=None):
-    raw = raw_get_anime_details(id)
+def get_anime_details(anime_id, mine=0, auth_token=None):
+    raw = raw_get_anime_details(anime_id, mine=mine, auth_token=auth_token)
     return helpers.json_to_anime(raw)
 
 
 def search_anime(query):
     raw = raw_search_anime(query)
-    return helpers.json_to_anime_list(raw)
+    return helpers.json_to_list_of_anime(raw)
 
 
 def get_manga_list(username):
@@ -164,11 +166,57 @@ def get_manga_list(username):
     return helpers.json_to_manga_list(raw)
 
 
-def get_manga_details(id, mine=0, auth_token=None):
-    raw = raw_get_manga_details(id)
+def get_manga_details(manga_id, mine=0, auth_token=None):
+    raw = raw_get_manga_details(manga_id, mine=mine, auth_token=auth_token)
     return helpers.json_to_manga(raw)
 
 
 def search_manga(query):
     raw = raw_search_manga(query)
-    return helpers.json_to_manga_list(raw)
+    return helpers.json_to_list_of_manga(raw)
+
+
+def add_anime_entry(anime_id, auth_token, status=1, episodes=0, score=None):
+    payload = {'anime_id': anime_id, 'status': status, 'episodes': episodes}
+    if score != None:
+        payload['score'] = score
+    response = requests.post(apiurl + '/animelist/anime', data=payload, auth=auth_token)
+
+
+def update_anime_entry(anime_id, auth_token, status=1, episodes=0, score=None):
+    payload = {'anime_id': anime_id, 'status': status, 'episodes': episodes}
+    if score != None:
+        payload['score'] = score
+    response = requests.put(apiurl + '/animelist/anime/' + str(anime_id), data=payload, auth=auth_token)
+
+
+def delete_anime_entry(anime_id, auth_token):
+    response = requests.delete(apiurl + '/animelist/anime/' + str(anime_id), auth=auth_token)
+    #TODO: Return an actual instance of the appropriate model here
+    return response
+
+
+def add_manga(manga_id, auth_token, status=1, chapters=0, volumes=0, score=None):
+    payload = {'manga_id': manga_id, 'status': status, 'chapters': chapters, 'volumes': volumes}
+    if score != None:
+        payload['score'] = score
+    response = requests.post(apiurl + '/mangalist/manga', data=payload, auth=auth_token)
+
+
+def update_manga_entry(manga_id, auth_token, status=0, chapters=0, volumes=0, score=None):
+    payload = {'manga_id': manga_id, 'status': status, 'chapters': chapters, 'volumes': volumes}
+    if score != None:
+        payload['score'] = score
+    response = requests.put(apiurl + '/mangalist/manga/' + str(manga_id), data=payload, auth=auth_token)
+    # TODO: Return an actual isntance of the appropriate model here
+
+
+def delete_manga_entry(manga_id, auth_token):
+    response = requests.delete(apiurl + '/mangalist/manga/' + str(manga_id), auth=auth_token)
+    #TODO: Return an actual instance of the appropriate model here
+    return response
+
+
+def verify_credentials(auth_token):
+    response = requests.get(apiurl + '/account/verify_credentials', auth=auth_token)
+    return response
